0f850a8d266745af4e6e1920e2308ebb0b9b267b
SilverHL <nelson.d.wang@gmail.com>
2018-08-12 14:18:49 +0800

    区间动归 (学习中)

--- dp/harder/mergeStone.cpp
+++ dp/harder/mergeStone.cpp
@@ -1,38 +1,70 @@
+/***
+ * 区间动规 
+ * 合并石子
+ * */
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int n;
-int f[1000][1000] = {};
-int sum[1000] = {};
-int a[1000] = {};
-int cost[1000][1000] = {};
-
+//dp[i][j] 用来表示从i 合并到j的最小代价
 int main()
 {
-    cin >> n;
-    for (int i = 1; i <= n; i++) 
+    int n, x;
+    int sum[200];
+    int dp[200][200];
+    int tmp;
+
+    sum[0] = 0;
+    for (int i = 1; i <= n; i++)
     {
-        cin >> a[i];
-        sum[i] = sum[i-1] + a[i];
+        cin >> tmp;
+        sum[i] = sum[i-1] + tmp;
+        dp[i][i] = 0;
     }
 
-    for (int i = 1; i <= n; i++) 
-        for (int j = i; j <= n; j++) 
-            cost[i][j] = sum[j] - sum[i-1];
-
-    for (int len = 2; len <= n; len++) 
-        for (int i = 1; i <= n; i++) 
+    for (int len = 2; len <= n; len++)
+        for (int i = 1; i <= n; i++)
         {
-            int j = i + len - 1;
-            if (j > n) 
+            int j = i + len-1;
+            if (j > n)
                 continue;
-            for (int k = i; k < j; k++) 
-                if ((f[i][k] + f[k+1][j] + cost[i][j] < f[i][j]) || !f[i][j]) 
-                    f[i][j] = f[i][k] + f[k+1][j] + cost[i][j];
+            for (int k = i; k < j; k++)
+                dp[i][j] = min(dp[i][j], dp[i][k]+dp[k+1][j] + sum[j] - sum[i-1]);
         }
 
-    cout << f[1][n];
-    return 0;
 }
 
+int main1()
+{
+    int n, x;
+    int sum[200];
+    int dp[200][200];
+    int tmp;
+    int s[200][200];
+
+    sum[0] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> tmp;
+        sum[i] = sum[i-1] + tmp;
+        dp[i][i] = 0;
+        s[i][i] = i; 
+    }
+
+    for (int len = 2; len <= n; len++)
+        for (int i = 1; i <= n; i++)
+        {   
+            int j = i + len -1;
+            if (j > n) 
+                continue;
+            for (int k = s[i][j-1]; k <= s[i+1][j]; k++)
+            {
+                if (dp[i][k] + dp[k+1][i] + sum[j] - sum[i-1] > dp[i][j])
+                {
+                    dp[i][j] = dp[i][k] + dp[k+1][j] + sum[j] - sum[i-1];
+                    s[i][j] = k;
+                }
+            }
+        }
+
+}

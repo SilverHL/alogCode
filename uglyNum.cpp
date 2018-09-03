6f4ff3c570251dc2311df533967b28213946ffe5
SilverHL <nelson.d.wang@gmail.com>
2018-09-03 19:22:26 +0800

    update

--- uglyNum.cpp
+++ uglyNum.cpp
@@ -9,26 +9,28 @@ int findMin(int i, int j, int k)
             (res == j ? 1 : 2));
 }
 
-int GetUglyNumber_Solution(int index) {
-    if (index <= 0) 
-        return -1;
+int GetUglyNumber_Solution(int index) 
+{
+    if (index <= 0)
+        return 0;
+
     int *a = new int[index];
     a[0] = 1;
-
-    int *pos = new int[3]{0, 0, 0};
-    int *num = new int[3]{2, 3, 5};
-    int *d = new int[3]{2, 3, 5};
+    int d[3] = {2, 3, 5};
+    int ans[3] = {2, 3, 5};
+    int pos[3] = {0, 0, 0};
 
     int cur = 1;
-    while (cur != index) {
-        int m = findMin(num[0], num[1], num[2]);
-        if (a[cur-1] < num[m]) 
-            a[cur++] = num[m];
-        pos[m] += 1;
-        num[m] = a[pos[m]] * d[m];
-
+    while (cur != index)
+    {
+        int m = findMin(ans[0], ans[1], ans[2]);
+        if (a[cur-1] < ans[m])
+            a[cur++] = ans[m];
+        pos[m]++;
+        ans[m] = a[pos[m]] * d[m];
     }
 
+    cout << a[index-1];
+    return a[index-1];
 
-    
 }
